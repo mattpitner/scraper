66f17dc1153493bdb574137d0886f8f33efc0aae
matt <mattpitner09@gmail.com>
2019-04-24 15:54:47 -0600

    scraper garbs summary

--- server.js
+++ server.js
@@ -33,29 +33,43 @@ mongoose.connect("mongodb://localhost/scraper", { useNewUrlParser: true });
 
 // A GET route for scraping the echoJS website
 app.get("/scrape", function(req, res) {
+  console.log("scrape")
   // First, we grab the body of the html with axios
   axios.get("https://thehardtimes.net/").then(function(response) {
     // Then, we load that into cheerio and save it to $ for a shorthand selector
     var $ = cheerio.load(response.data);
 
     // Now, we grab every h2 within an article tag, and do the following:
-    $("article h2").each(function(i, element) {
+    $("article").each(function(i, element) {
       // Save an empty result object
       var result = {};
 
-      // Add the text and href of every link, and save them as properties of the result object
-      result.title = $(this)
-        .children("a")
+      // result.title = $(this)
+      //   .children("a")
+      //   .text();
+      // result.link = $(this)
+      //   .children("a")
+      //   .attr("href");
+      // result.summary = $(this)
+      //   .children("p")
+      //   .text();
+
+        result.title = $(this)
+        .find("a")
         .text();
       result.link = $(this)
-        .children("a")
+        .find("a")
         .attr("href");
-
+      result.summary = $(this)
+        .find("p")
+        .text();
+    console.log(result);
       // Create a new Article using the `result` object built from scraping
+      console.log("-------------")
       db.Article.create(result)
         .then(function(dbArticle) {
           // View the added result in the console
-          console.log(dbArticle);
+          console.log("inserted: ", dbArticle);
         })
         .catch(function(err) {
           // If an error occurred, log it
